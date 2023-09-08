fb6620fd3e8b55125016a8a8ee31dcf4b56c9cdf
Niko <rappe.niko@gmail.com>
2023-09-08 19:00:51 +0300

    Exercise 13.3

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model, DataTypes, DATE } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+class Blog extends Model {}
+
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
 
 const main = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
+    const blogs = await Blog.findAll()
+    blogs.map(b => {
+      console.log(`Blog: ${b.dataValues.title}, read at: ${b.dataValues.url}. Likes: ${b.dataValues.likes} `)
+    })
     sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
