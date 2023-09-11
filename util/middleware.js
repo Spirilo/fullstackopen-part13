b43fea605e19af122f64cb62d24378d4421fd3a0
Niko <rappe.niko@gmail.com>
2023-09-11 14:08:56 +0300

    Exercise 13.10

--- util/middleware.js
+++ util/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+
+const { SECRET } = require('../util/config')
 const { Blog } = require('../models')
 
 const errorHandler = (error, request, response, next) => {
@@ -21,7 +24,24 @@ const blogFinder = async (req, res, next) => {
   next()
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      console.log(authorization.substring(7))
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error){
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
 module.exports = {
   errorHandler,
-  blogFinder
+  blogFinder,
+  tokenExtractor
 }
