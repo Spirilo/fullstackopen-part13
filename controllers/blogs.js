144455e9325b9337ac26244ec8f951394fdeb1e8
Niko <rappe.niko@gmail.com>
2023-09-11 17:15:52 +0300

    Exercise 13.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,25 @@
 const router = require('express').Router()
+const { Op } = require('sequelize')
 
 const { Blog, User } = require('../models')
 const { blogFinder, tokenExtractor } = require('../util/middleware')
 
 router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.substring]: req.query.search
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name', 'username']
-    }
+    },
+    where
   })
   res.json(blogs)
 })
